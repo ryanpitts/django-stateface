4853d664c7d213c74ac20810586e10c815a687f4
Ryan Pitts <ryan.a.pitts@gmail.com>
2012-03-22 17:07:48 -0700

    now accepting state full names, postal codes or AP abbreviations

--- stateface/templatetags/stateface.py
+++ stateface/templatetags/stateface.py
@@ -4,100 +4,85 @@ font project.
 
     http://propublica.github.com/stateface/
 
-This tag assumes you have the StateFace font installed on your webserver,
-and a @font-face declaration and CSS class something like:
-
-    @font-face {
-        font-family: 'StateFaceRegular';
-        src: url('<YOUR_PATH_HERE>/stateface-regular-webfont.eot');
-        src: url('<YOUR_PATH_HERE>/stateface-regular-webfont.eot?#iefix') format('embedded-opentype'),
-             url('<YOUR_PATH_HERE>/stateface-regular-webfont.woff') format('woff'),
-             url('<YOUR_PATH_HERE>/stateface-regular-webfont.ttf') format('truetype'),
-             url('<YOUR_PATH_HERE>/stateface-regular-webfont.svg#StateFaceRegular') format('svg');
-        font-weight: normal;
-        font-style: normal;
-    }
-
-    i.stateface {font-family: 'StateFaceRegular'; font-style: normal;}
-    
-Adjust the `templates/stateface/stateface_icon.html` as you see fit, to change
-the CSS class, use a different HTML element to contain the shape, whatever.
-
-USAGE:
-
-In your Django templates, render a state shape icon by feeding this tag with
+In your Django templates, render a state shape icon by feeding this tag
 a string or a context variable.
 
+    {% render_stateface_icon 'Washington' %}
     {% render_stateface_icon 'WA' %}
+    {% render_stateface_icon 'Wash.' %}
     {% render_stateface_icon restaurant.city.state.abbr %}
-
+    
 '''
 from django import template
 
 register = template.Library()
 
-STATE_KEY_DICT = {
-  "GA": "J",
-  "OR": "k",
-  "OK": "j",
-  "OH": "i",
-  "DC": "y",
-  "DE": "H",
-  "VA": "s",
-  "VT": "t",
-  "LA": "R",
-  "AR": "C",
-  "AZ": "D",
-  "AK": "A",
-  "AL": "B",
-  "SD": "o",
-  "SC": "n",
-  "IA": "L",
-  "IN": "O",
-  "IL": "N",
-  "ID": "M",
-  "FL": "I",
-  "ND": "b",
-  "NC": "a",
-  "NY": "h",
-  "NM": "f",
-  "NJ": "e",
-  "NH": "d",
-  "NV": "g",
-  "NE": "c",
-  "CT": "G",
-  "CO": "F",
-  "CA": "E",
-  "US": "z",
-  "UT": "r",
-  "KY": "Q",
-  "KS": "P",
-  "RI": "m",
-  "HI": "K",
-  "PA": "l",
-  "WY": "x",
-  "WI": "v",
-  "WV": "w",
-  "WA": "u",
-  "MT": "Z",
-  "MO": "X",
-  "MS": "Y",
-  "MN": "W",
-  "MI": "V",
-  "MA": "S",
-  "MD": "T",
-  "ME": "U",
-  "TX": "q",
-  "TN": "p"
-}
+STATE_KEY_MAP = [
+    ["Alabama", "AL", "Ala.", "B"],
+    ["Alaska", "AK", "Alaska", "A"],
+    ["Arizona", "AZ", "Ariz.", "D"],
+    ["Arkansas", "AR", "Ark.", "C"],
+    ["California", "CA", "Calif.", "E"],
+    ["Colorado", "CO", "Colo.", "F"],
+    ["Connecticut", "CT", "Conn.", "G"],
+    ["Delaware", "DE", "Del.", "H"],
+    ["District of Columbia", "DC", "D.C.", "y"],
+    ["Florida", "FL", "Fla.", "I"],
+    ["Georgia", "GA", "Ga.", "J"],
+    ["Hawaii", "HI", "Hawaii", "K"],
+    ["Idaho", "ID", "Idaho", "M"],
+    ["Illinois", "IL", "Ill.", "N"],
+    ["Indiana", "IN", "Ind.", "O"],
+    ["Iowa", "IA", "Iowa", "L"],
+    ["Kansas", "KS", "Kan.", "P"],
+    ["Kentucky", "KY", "Ky.", "Q"],
+    ["Louisiana", "LA", "La.", "R"],
+    ["Maine", "ME", "Maine", "U"],
+    ["Maryland", "MD", "Md.", "T"],
+    ["Massachusetts", "MA", "Mass.", "S"],
+    ["Michigan", "MI", "Mich.", "V"],
+    ["Minnesota", "MN", "Minn.", "W"],
+    ["Mississippi", "MS", "Miss.", "Y"],
+    ["Missouri", "MO", "Mo.", "X"],
+    ["Montana", "MT", "Mont.", "Z"],
+    ["Nebraska", "NE", "Neb.", "c"],
+    ["Nevada", "NV", "Nev.", "g"],
+    ["New Hampshire", "NH", "N.H.", "d"],
+    ["New Jersey", "NJ", "N.J.", "e"],
+    ["New Mexico", "NM", "N.M.", "f"],
+    ["New York", "NY", "N.Y.", "h"],
+    ["North Carolina", "NC", "N.C.", "a"],
+    ["North Dakota", "ND", "N.D.", "b"],
+    ["Ohio", "OH", "Ohio", "i"],
+    ["Oklahoma", "OK", "Okla.", "j"],
+    ["Oregon", "OR", "Ore.", "k"],
+    ["Pennsylvania", "PA", "Pa.", "l"],
+    ["Rhode Island", "RI", "R.I.", "m"],
+    ["South Carolina", "SC", "S.C.", "n"],
+    ["South Dakota", "SD", "S.D.", "o"],
+    ["Tennessee", "TN", "Tenn.", "p"],
+    ["Texas", "TX", "Texas", "q"],
+    ["Utah", "UT", "Utah", "r"],
+    ["Vermont", "VT", "Vt.", "t"],
+    ["Virginia", "VA", "Va.", "s"],
+    ["Washington", "WA", "Wash.", "u"],
+    ["West Virginia", "WV", "W.Va.", "w"],
+    ["Wisconsin", "WI", "Wis.", "v"],
+    ["Wyoming", "WY", "Wyo.", "x"],
+    ["United States", "US", "U.S.", "z"],
+]
 
 @register.inclusion_tag('stateface/stateface_icon.html', takes_context=True)
-def render_stateface_icon(context, postal_code):
-    state_key = context.get('postal_code', None)
-    if state_key:
-        # have we been passed a var?
-        return {'state_key': STATE_KEY_DICT[state_key]}
-    else:
-        # nope, we've been passed a string
-        return {'state_key': STATE_KEY_DICT[postal_code]}
+def render_stateface_icon(context, state_value):
+    # check for context variable, fall back to string input
+    var = context.get(state_value, state_value)
     
+    # find state in map based on full name, postal code or AP abbrevation
+    state_list = filter(lambda state: var in state, STATE_KEY_MAP)
+
+    # if we find a state, the last value is the StateFace keycode
+    if len(state_list):
+        state_key = state_list[0][3]
+    else:
+        state_key = ''
+    return {'state_key': state_key}
